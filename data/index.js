e9d60bae5c0c85ae153e1a57056cd3136ec61741
siva <siva.mca19@gmail.com>
2022-07-14 12:49:09 +0530

    added student data

--- data/index.js
+++ data/index.js
@@ -1,2 +1,32 @@
 
-module.exports = "Hello World"
+const students = [
+  {
+    id: 1,
+    name: 'Siva',
+    marks: {
+      english: 100,
+      science: 100,
+      social: 100,
+    }
+  },
+   {
+    id: 2,
+    name: 'mani',
+    marks: {
+      english: 80,
+      science: 75,
+      social: 89,
+    }
+  },
+  {
+    id: 3,
+    name: 'Karthick',
+    marks: {
+      english: 90,
+      science: 85,
+      social: 90,
+    }
+  }
+]
+
+module.exports = students

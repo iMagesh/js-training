d98ed8108005e583d2a7b802ac100e4b751fabf6
siva <siva.mca19@gmail.com>
2022-07-14 14:41:29 +0530

    added calculate the total func

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const data = require('./data')
 
+const calculateTotal = (student) => {
+  const marks = Object.values(student.marks)
+  student.totalMark = marks.reduce((mark, value) => value + mark , 0)
+  return student
+}
+ 
 const transformStudent = (student) =>{
-  const { english, social, science}  = student.marks
-  return `${student.id}, ${student.name}, ${english} ${social} ${science}`
+  const { id, name, totalMark, marks: {english, social, science} }  = student
+  return `${id}, ${name}, ${english} ${social} ${science}: ${totalMark}`
 }
 
 const logResult = (result) => console.log(result)
 
 const main = () => {
-  data.map(transformStudent).map(logResult)
+  data
+    .map(calculateTotal)
+    .map(transformStudent)
+    .map(logResult)
 }
 
 main()

726871098f645b5067ca2cd8e27f78551887570b
siva <siva.mca19@gmail.com>
2022-07-14 14:59:03 +0530

    refactor the calculate method

--- index.js
+++ index.js
@@ -2,20 +2,26 @@ const data = require('./data')
 
 const calculateTotal = (student) => {
   const marks = Object.values(student.marks)
-  student.totalMark = marks.reduce((mark, value) => value + mark , 0)
-  return student
+  const total = marks.reduce((mark, value) => value + mark , 0)
+  return { ...student, total }
 }
+
+const refactorCalculateTotal = (student) => 
+  ({
+    ...student, 
+    total: Object.values(student.marks).reduce((mark, value) => value + mark, 0) 
+  })
  
 const transformStudent = (student) =>{
-  const { id, name, totalMark, marks: {english, social, science} }  = student
-  return `${id}, ${name}, ${english} ${social} ${science}: ${totalMark}`
+  const { id, name, total, marks: {english, social, science} }  = student
+  return `${id}, ${name}, ${english} ${social} ${science}: ${total}`
 }
 
 const logResult = (result) => console.log(result)
 
 const main = () => {
   data
-    .map(calculateTotal)
+    .map(refactorCalculateTotal)
     .map(transformStudent)
     .map(logResult)
 }

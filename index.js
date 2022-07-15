e770a7b454bc66a6cebc99e9159bc67bb645426f
siva <siva.mca19@gmail.com>
2022-07-15 11:22:39 +0530

    reorder the method & removed the unused methods

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const data = require('./data')
 
+const displayStudentMarkListTable = (students) => {
+  const transformed = students.map(({marks, ...rest}) => ({ ...rest, ...marks }))
+  console.table(transformed)
+}
+
+const generateRank = (students) => {
+  return students
+    .filter((student) => student.isPassed)
+    .sort((a,b) => b.total - a.total)
+    .map((student, index) => ({ ...student, rank: index + 1 }))
+    .concat(students.filter((student) => !student.isPassed))
+}
+
 const processMarkSheet = (student) => 
   {
     const marks = Object.values(student.marks)
@@ -10,35 +23,13 @@ const processMarkSheet = (student) =>
       isPassed
     })
   }
- 
-const transformStudent = (student) =>{
-  const { id, name, total, isPassed, marks: {english, social, science}, rank }  = student
-  return `${id}, ${name}, ${english} ${social} ${science}: 
-    ${total} ${isPassed ? 'Pass' : 'Fail'} Rank: ${rank || 'F'}`
-}
-
-const logResult = (result) => console.table(result)
 
 const processStudentMark = (students) => students.map(processMarkSheet)
-
-const generateRank = (students) => {
-  return students
-    .filter((student) => student.isPassed)
-    .sort((a,b) => b.total - a.total)
-    .map((student, index) => ({ ...student, rank: index + 1 }))
-    .concat(students.filter((student) => !student.isPassed))
-}
-
-const displayStudentMarkList = (students) => {
-  // students.map(transformStudent).map(logResult)
-  const transformed = students.map(({marks, ...rest}) => ({ ...rest, ...marks }))
-  console.table(transformed)
-}
 
 const main = () => {
   const processedStudentReports = processStudentMark(data)
   const processedRanksData = generateRank(processedStudentReports)
-  displayStudentMarkList(processedRanksData)
+  displayStudentMarkListTable(processedRanksData)
 }
 
 main()

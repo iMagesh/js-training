6fc923aceb3b4f68fdf5fd1ba7cb2499624fc8b0
siva <siva.mca19@gmail.com>
2022-07-15 12:35:58 +0530

    refactor code, update the method name more readable

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const data = require('./data')
 
-const capitalizeString = (string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
+const capitalizeString = (string) => string.charAt(0).toUpperCase() + string.slice(1)
 
 const formatStudent = ({isPassed, marks, ...rest}) => {
   const student = {...rest, ...marks, result: isPassed ? 'Pass' : 'Fail'}
+
   return Object.keys(student).reduce((obj, key) => {
     obj[capitalizeString(key)] = student[key]
     return obj
@@ -17,32 +16,33 @@ const displayStudentMarkListTable = (students) => {
   console.table(transformedStudents)
 }
 
-const generateRank = (students) => {
+const generateStudentsRank = (students) => {
   const passedStudents = students.filter((student) => student.isPassed)
   const failedStudents = students.filter((student) => !student.isPassed)
   const rankedStudents = passedStudents
     .sort((a,b) => b.total - a.total)
     .map((student, index) => ({ ...student, rank: index + 1 }))
+
   return rankedStudents.concat(failedStudents)
 }
 
-const processMarkSheet = (student) => 
-  {
-    const marks = Object.values(student.marks)
-    const isPassed = marks.every(mark => mark >= 40);
-    return ({
-      ...student,
-      total: marks.reduce((mark, value) => value + mark, 0),
-      isPassed
-    })
-  }
+const processMarkSheet = (student) => {
+  const marks = Object.values(student.marks)
+  const isPassed = marks.every(mark => mark >= 40);
 
-const processStudentMark = (students) => students.map(processMarkSheet)
+  return ({
+    ...student,
+    total: marks.reduce((mark, value) => value + mark, 0),
+    isPassed
+  })
+}
 
-const main = () => {
-  const processedStudentReports = processStudentMark(data)
-  const processedRanksData = generateRank(processedStudentReports)
+const processStudentMarks = (students) => students.map(processMarkSheet)
+
+const main = (students) => {
+  const processedStudentReports = processStudentMarks(students)
+  const processedRanksData = generateStudentsRank(processedStudentReports)
   displayStudentMarkListTable(processedRanksData)
 }
 
-main()
+main(data)
